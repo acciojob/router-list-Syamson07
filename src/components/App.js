4d583754cd5d5a948b9ed9e3a62e78e85b0313e1
Syam <syam.penumala225@gmail.com>
2025-06-04 21:48:36 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
